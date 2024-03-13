ced5b7d118935938d2b5c04f4d6f03508c6bc1d3
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-03-13 14:04:03 -0400

    Working on WeatherApp

---
+++ WeatherApp/src/components/UpcomingWeather.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
+
+const UpcomingWeather = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+        <Text>Upcoming Weather</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
+
+export default UpcomingWeather
+
+// https://www.youtube.com/watch?v=obH0Po_RdWk
